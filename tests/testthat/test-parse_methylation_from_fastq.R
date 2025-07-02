af88b7254cb800e803d35debc3e7cf9f097b03cd
evelyn <evelynjade42@gmail.com>
2025-07-02 17:43:31 +1200

    Finished documenting and testing writing modification FASTQ

--- tests/testthat/test-parse_methylation_from_fastq.R
+++ tests/testthat/test-parse_methylation_from_fastq.R
@@ -1,3 +1,56 @@
+root <- "test_output_data/"
+reference <- "reference_output_data/"
+
+
+## WRITING TO FASTQ
+## -------------------------------------------------------------------------------------
+test_that("writing modification info to fastq works", {
+    filename <- "example_many_sequences.fastq"
+    write_modified_fastq(example_many_sequences, paste0(root, filename))
+    expect_equal(readLines(paste0(root, filename)), readLines(paste0(reference, filename)))
+})
+
+test_that("writing modification info to fastq rejects bad arguments", {
+    # "x" is a bad value because there isn't a colname called that
+    bad_param_value_for_single_colname <- list(NULL, NA, c("sequence", "read"), 1, TRUE, -1, 0, 2.5, "x", character())
+    for (param in bad_param_value_for_single_colname) {
+        expect_error(write_modified_fastq(example_many_sequences, read_id_colname = param), class = "argument_value_or_type")
+        expect_error(write_modified_fastq(example_many_sequences, sequence_colname = param), class = "argument_value_or_type")
+    }
+
+    bad_param_value_for_single_colname_NA_allowed <- list(NULL, c("sequence", "read"), 1, TRUE, -1, 0, 2.5, "x", character())
+    for (param in bad_param_value_for_single_colname_NA_allowed) {
+        expect_error(write_modified_fastq(example_many_sequences, quality_colname = param), class = "argument_value_or_type")
+    }
+
+    bad_param_value_for_multiple_colname <- list(NULL, NA, c("y", "read"), 1, TRUE, -1, 0, 2.5, "x", character(), c(1, 0))
+    for (param in bad_param_value_for_multiple_colname) {
+        expect_error(write_modified_fastq(example_many_sequences, locations_colnames = param), class = "argument_value_or_type")
+        expect_error(write_modified_fastq(example_many_sequences, probabilities_colnames = param), class = "argument_value_or_type")
+    }
+
+    bad_param_value_for_multiple_char <- list(NULL, NA, 1, TRUE, -1, 0, 2.5, c(1, 0), character())
+    for (param in bad_param_value_for_multiple_char) {
+        expect_error(write_modified_fastq(example_many_sequences, modification_prefixes = param), class = "argument_value_or_type")
+    }
+
+    bad_param_value_for_filename <- list(NULL, 1, TRUE, -1, 0, 2.5, c(1, 0), character())
+    for (param in bad_param_value_for_filename) {
+        expect_error(write_modified_fastq(example_many_sequences, filename = param), class = "argument_value_or_type")
+    }
+
+    bad_param_value_for_logical <- list("X", c(TRUE, FALSE), NA, NULL, 1, -1, 0.5)
+    for (param in bad_param_value_for_logical) {
+        expect_error(write_modified_fastq(example_many_sequences, return = param), class = "argument_value_or_type")
+    }
+
+    expect_error(write_modified_fastq(example_many_sequences,
+                                      locations_colnames = c("methylation_locations"),
+                                      probabilities_colnames = c("methylation_probabilities"),
+                                      modification_prefixes = c("C+h?", "C+m?")),
+                 class = "argument_value_or_type")
+})
+
 test_that("helper function for constructing MM vector works", {
     expect_equal(convert_locations_to_MM_vector("GGCGGCGGC", c(6,9), "C"),
                  c(1, 0))
@@ -20,10 +73,10 @@ test_that("helper function for constructing MM vector fails", {
         expect_error(convert_locations_to_MM_vector("CGGCGGCGGC", c(1, 4, 7, 10), param), class = "argument_value_or_type")
     }
 
+    ## 3 and 9 are bad values only because there isn't a "C" at those indices.
     bad_param_value_for_locations <- list("x", character(), NULL, NA, 0, 0.5, -1, c(3, 9), 3, 9)
     for (param in bad_param_value_for_locations) {
         expect_error(convert_locations_to_MM_vector("CGGCGGCGGC", param, "C"), class = "argument_value_or_type")
     }
-
-
 })
+## -------------------------------------------------------------------------------------
