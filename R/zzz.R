70b98b52dda0837480afb57a862cba89b3720cd2
evelyn <evelynjade42@gmail.com>
2025-11-18 16:38:15 +1300

    Does adding extra environment specification help with delayed assignment

--- R/zzz.R
+++ R/zzz.R
@@ -5,11 +5,13 @@
     delayedAssign(
         "sequence_color_palettes",
         sequence_colour_palettes,
-        assign.env = as.environment(pkg_namespace)
+        assign.env = as.environment(pkg_namespace),
+        eval.env = as.environment(pkg_namespace)
     )
     delayedAssign(
         "sequence_col_palettes",
         sequence_colour_palettes,
-        assign.env = as.environment(pkg_namespace)
+        assign.env = as.environment(pkg_namespace),
+        eval.env = as.environment(pkg_namespace)
     )
 }
