54b65938854708f5af9ae6cd634cf9a18254696a
evelyn <evelynjade42@gmail.com>
2026-01-13 12:43:34 +1300

    Removed manifest from itself to prevent infinite loop

--- inst/shinyapp/create_manifest.R
+++ inst/shinyapp/create_manifest.R
@@ -1,11 +1,16 @@
 ## Must be run in shinyapp directory
 if (file.exists("app.R")) {
+    app_files <- system("cd ../..; git ls-files", intern = TRUE)
+    app_files <- app_files[app_files != "manifest.json"]
+    app_files <- sort(app_files)
+    
     rsconnect::writeManifest(
         appDir = "../..",
-        appFiles = system("cd ../..; git ls-files", intern = TRUE),
+        appFiles = ,
         appPrimaryDoc = "inst/shinyapp/app.R",
         verbose = TRUE
     )
 } else {
     abort("Not in correct directory")
 }
+
